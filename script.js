897adfa37da3ec78e6bdbcfa14d3dcf8a0241132
EdwardNew <pkarrows@gmail.com>
2022-10-23 22:27:16 -0700

    Animations update: added animations for image gallery on scroll and hamburger menu

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+ * Image gallery animation
+ * Credit: Beyond Fireship: https://www.youtube.com/watch?v=T33NN_pPeNI
+ */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
